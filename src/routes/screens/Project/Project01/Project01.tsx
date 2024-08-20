a3bda9a3ac684dc5522cd8149cf46b65d1605066
Kim Gang Yeon <rkddus3177@naver.com>
2024-08-21 02:58:22 +0900

    앗

--- src/routes/screens/Project/Project01/Project01.tsx
+++ src/routes/screens/Project/Project01/Project01.tsx
@@ -1,11 +1,52 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, Dimensions} from 'react-native';
 import React from 'react';
+import styled from 'styled-components/native';
+
+const Container = styled.ScrollView``;
+
+const ImageStart = styled.View`
+  position: relative;
+  flex: 1;
+  background-color: red;
+`;
+
+const Header = styled.View`
+  position: absolute;
+  z-index: 1;
+  top: 25px;
+  width: 100%;
+`;
+
+const HeaderText = styled.Text`
+  font-weight: bold;
+  color: white;
+  font-size: 30px;
+  text-align: center;
+  letter-spacing: 3px;
+`;
 
 const Project01 = () => {
+  const windowHeight = Dimensions.get('window').height;
+  const windowWidth = Dimensions.get('window').width;
+
   return (
-    <View>
-      <Text>Project01</Text>
-    </View>
+    <>
+      <Container>
+        <ImageStart>
+          <Image
+            source={require('../../../../assets/project01/pro1.jpg')}
+            resizeMode="cover"
+            style={{height: windowHeight}}
+          />
+        </ImageStart>
+        <View style={{height: windowHeight}}>
+          <Text>hello</Text>
+        </View>
+      </Container>
+      <Header>
+        <HeaderText>CHANEL</HeaderText>
+      </Header>
+    </>
   );
 };
 
